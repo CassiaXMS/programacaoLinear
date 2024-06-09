a0f5e3562176c43c430d919c9fe600abed13b397
Cássia Xavier Mendes dos Santos <99376161+CassiaXMS@users.noreply.github.com>
2024-06-09 15:47:27 -0300

    Update programacaoLinear.py

--- programacaoLinear.py
+++ programacaoLinear.py
@@ -1,58 +1,42 @@
+'''-----EXERCÍCIO PRGRAMAÇÃO LINEAR COM MATRIZES--------------'''
 
-'''''-----SOLUÇÃO NÃO UTILIZANDO MATRIZ,BIBILIOTECA PULP--------------''''
-
-'''import pulp
-
-# Criação do problema
-problema = pulp.LpProblem("Maximizar_Lucro", pulp.LpMaximize)
-
-# Variáveis de decisão
-x1 = pulp.LpVariable('P1', lowBound=0, cat='Continuous')
-x2 = pulp.LpVariable('P2', lowBound=0, cat='Continuous')
-
-# Função objetivo
-problema += 1900 * x1 + 2100 * x2, "Lucro_Total"
-
-# Restrições
-problema += 4 * x1 + 2 * x2 <= 20, "Forja"
-problema += 2 * x1 + 3 * x2 <= 10, "Polimento"
-problema += 100 * x1 + 200 * x2 <= 500, "Materia_Prima"
-
-# Resolução do problema
-problema.solve()
-
-# Exibição dos resultados
-print("Status:", pulp.LpStatus[problema.status])
-print("P1 (quantidade de produto P1 a ser produzido):", pulp.value(x1))
-print("P2 (quantidade de produto P2 a ser produzido):", pulp.value(x2))
-print("Lucro Total:", pulp.value(problema.objective))'''
-
-'''''-----SOLUÇÃO UTILIZANDO MATRIZ BIBILIOTECA SCIPY--------------''''
+'''EXERCÍCIO: 
+       
+        Função objetivo: Max L = 1900 x1 + 2100 x2
+        
+        Sujeito à:                  4x1+ 2x2 <= 20
+                                    2x1+ 3x2 <= 10
+                                    100x1 + 200x2 <=500
+                
+        Não negatividade:           x1 >= 0; x2 => 0
+'''
 
 from scipy.optimize import linprog
 
-# Coeficientes da função objetivo (negativos para maximização)
-c = [-1900, -2100]
+
+# Coeficientes da FUNÇÃO OBJETIVO (negativos para maximização)
+coeficientes = [-1900, -2100]
+
 
 # Matriz de coeficientes das restrições
-A = [
+matriz_restricoes = [
     [4, 2],
     [2, 3],
     [100, 200]
 ]
 
 # Vetor de limites das restrições
-b = [20, 10, 500]
+vetor_restricoes = [20, 10, 500]
 
 # Limites das variáveis (x1 >= 0, x2 >= 0)
 x_bounds = (0, None)
 bounds = [x_bounds, x_bounds]
 
 # Resolução do problema
-result = linprog(c, A_ub=A, b_ub=b, bounds=bounds, method='highs')
+resultado = linprog(coeficientes, A_ub=matriz_restricoes, b_ub=vetor_restricoes, bounds=bounds, method='highs')
 
 # Exibição dos resultados
-print("Status:", result.message)
-print("P1 (quantidade de produto P1 a ser produzido):", result.x[0])
-print("P2 (quantidade de produto P2 a ser produzido):", result.x[1])
-print("Lucro Total:", -result.fun)  # Negar o valor para obter o lucro máximo
+print("\n SOLUÇÃO ÓTIMA: \n")
+print(" Qtd. do P1 a ser produzido: ", resultado.x[0])
+print(" Qtd. do P2 a ser produzido: ", resultado.x[1])
+print(" Lucro Total: ", -resultado.fun)  # Negar o valor para obter o lucro máximo
